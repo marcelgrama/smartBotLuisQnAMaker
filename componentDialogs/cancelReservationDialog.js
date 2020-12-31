f1d5bf033dd409be4ce52fdee735e8c34bead443
gramam <marcel.grama@paddypowerbetfair.com>
2020-12-31 16:29:23 +0200

    init commit

---
+++ componentDialogs/cancelReservationDialog.js
@@ -0,0 +1,87 @@
+const { WaterfallDialog, ComponentDialog } = require('botbuilder-dialogs');
+
+const { DialogSet, DialogTurnStatus, ConfirmPrompt, ChoicePrompt, DateTimePrompt, NumberPrompt, TextPrompt } = require('botbuilder-dialogs');
+const { CardFactory } = require('botbuilder');
+const RestaurantCard = require('../resources/adaptiveCards/RestaurantCard');
+
+const CARDS = [RestaurantCard];
+
+const CHOICE_PROMPT = 'CHOICE_PROMPT';
+const CONFIRM_PROMPT = 'CONFIRM_PROMPT';
+const TEXT_PROMPT = 'TEXT_PROMPT';
+const NUMBER_PROMPT = 'NUMBER_PROMPT';
+const DATETIME_PROMPT = 'DATETIME_PROMPT';
+const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
+let endDialog = '';
+
+class CancelReservationDialog extends ComponentDialog {
+  constructor(conversationState, userState) {
+    super('cancelReservationDialog');
+
+    this.addDialog(new TextPrompt(TEXT_PROMPT));
+    this.addDialog(new ChoicePrompt(CHOICE_PROMPT));
+    this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT));
+    this.addDialog(new NumberPrompt(NUMBER_PROMPT));
+    this.addDialog(new DateTimePrompt(DATETIME_PROMPT));
+
+    this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
+      this.firstStep.bind(this), // Ask confirmation if user wants to make reservation?
+      this.confirmStep.bind(this), // show summary of values entered by user and ask confirmation to make reservation
+      this.summaryStep.bind(this), // 
+    ]));
+
+
+    this.initialDialogId = WATERFALL_DIALOG;
+
+  }
+
+  async run(turnContext, accessor) {
+    const dialogSet = new DialogSet(accessor);
+    dialogSet.add(this);
+    const dialogContext = await dialogSet.createContext(turnContext);
+
+    const results = await dialogContext.continueDialog();
+    if (results.status === DialogTurnStatus.empty) {
+      await dialogContext.beginDialog(this.id);
+    }
+
+  }
+
+  async firstStep(step) {
+    endDialog = false;
+    await step.context.sendActivity({
+      text: 'Enter reservation details for cancellation:',
+      attachments: [CardFactory.adaptiveCard(CARDS[0])]
+    })
+    //running a prompt here means the next waterfallStep will be run when the users response is receiveed.
+    return await step.prompt(TEXT_PROMPT, '');
+
+  }
+
+  async confirmStep(step) {
+    step.values.reservationNo = step.result;
+
+    const msg = ` You have entered following values: \n Reservation Number: ${step.values.reservationNo}`;
+    await step.context.sendActivity(msg);
+    return await step.prompt(CONFIRM_PROMPT, 'Are you sure that all values are correct and you want to CANCEL the reservation?', ['yes', 'no']);
+
+
+  }
+  async summaryStep(step) {
+
+    if (step.result === true) {
+      // business 
+      await step.context.sendActivity('Reservation successfully cancelled');
+      endDialog = true;
+      return await step.endDialog();
+    }
+
+  }
+
+
+  async isDialogComplete() {
+    return endDialog;
+  }
+}
+
+module.exports.CancelReservationDialog = CancelReservationDialog;
---
+++ rrbot.js
@@ -0,0 +1,124 @@
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+const { MakeReservationDialog } = require('./componentDialogs/makeReservationDialog');
+const { CancelReservationDialog } = require('./componentDialogs/cancelReservationDialog')
+
+
+
+class RRBOT extends ActivityHandler {
+    constructor(conversationState, userState) {
+        super();
+
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialogState = conversationState.createProperty("dialogState");
+        this.makeReservationDialog = new MakeReservationDialog(this.conversationState, this.userState);
+        this.cancelReservationDialog = new CancelReservationDialog(this.conversationState, this.userState);
+
+
+        this.previousIntent = this.conversationState.createProperty("previousIntent");
+        this.conversationData = this.conversationState.createProperty('conservationData');
+
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+
+            await this.dispatchToIntentAsync(context);
+
+            await next();
+
+        });
+
+        this.onDialog(async (context, next) => {
+            // Save any state changes. The load happened during the execution of the Dialog.
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+            await next();
+        });
+        this.onMembersAdded(async (context, next) => {
+            await this.sendWelcomeMessage(context)
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+
+
+
+    async sendWelcomeMessage(turnContext) {
+        const { activity } = turnContext;
+
+        // Iterate over all new members added to the conversation.
+        for (const idx in activity.membersAdded) {
+            if (activity.membersAdded[idx].id !== activity.recipient.id) {
+                const welcomeMessage = `Welcome to Restaurant Reservation Bot ${activity.membersAdded[idx].name}. `;
+                await turnContext.sendActivity(welcomeMessage);
+                await this.sendSuggestedActions(turnContext);
+            }
+        }
+    }
+
+    async sendSuggestedActions(turnContext) {
+        const reply = MessageFactory.suggestedActions(['Make Reservation', 'Cancel Reservation', 'Restaurant Address'], 'What would you like to do today ?');
+        await turnContext.sendActivity(reply);
+    }
+
+
+    async dispatchToIntentAsync(context) {
+
+        let currentIntent = '';
+        const previousIntent = await this.previousIntent.get(context, {});
+        const conversationData = await this.conversationData.get(context, {});
+
+        if (previousIntent.intentName && conversationData.endDialog === false) {
+            currentIntent = previousIntent.intentName;
+
+        }
+        else if (previousIntent.intentName && conversationData.endDialog === true) {
+            currentIntent = context.activity.text;
+
+        }
+        else {
+            currentIntent = context.activity.text;
+            await this.previousIntent.set(context, { intentName: context.activity.text });
+
+        }
+        switch (currentIntent) {
+
+            case 'Make Reservation':
+                console.log("Inside Make Reservation Case");
+                await this.conversationData.set(context, { endDialog: false });
+                await this.makeReservationDialog.run(context, this.dialogState);
+                conversationData.endDialog = await this.makeReservationDialog.isDialogComplete();
+                if (conversationData.endDialog) {
+                    await this.previousIntent.set(context, { intentName: null });
+                    await this.sendSuggestedActions(context);
+
+                }
+                break;
+
+
+            case 'Cancel Reservation':
+                console.log("Inside Cancel Reservation Case");
+                await this.conversationData.set(context, { endDialog: false });
+                await this.cancelReservationDialog.run(context, this.dialogState);
+                conversationData.endDialog = await this.cancelReservationDialog.isDialogComplete();
+                if (conversationData.endDialog) {
+                    await this.previousIntent.set(context, { intentName: null });
+                    await this.sendSuggestedActions(context);
+
+                }
+
+                break;
+
+
+            default:
+                console.log("Did not match Make Reservation case");
+                break;
+        }
+
+
+    }
+
+
+}
+
+module.exports.RRBOT = RRBOT;
